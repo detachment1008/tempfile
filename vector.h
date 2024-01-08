735ce89ae66e5b09b248cd0eeaa2a4bd5efc4b01
dcr <974449413@qq.com>
2024-01-08 14:24:30 +0800

    first

---
+++ vector.h
@@ -0,0 +1,64 @@
+#include <cstddef>
+
+template <typename T>
+class vector
+{
+public:
+    /*
+       容量相关
+   */
+
+    // 空间相关(不会修改元素)
+    void reserve(size_t n); // 重新申请空间，只能多不能少(指的是空间大小而非元素个数)
+    void shrink_to_fit();   // 重新申请空间，大小和当前元素的个数相同
+    size_t capacity();      // 空间大小
+    // 元素相关(不会直接修改空间，按正常空间增长模式进行，不会减小空间)
+    void resize(size_t sz); // 直接设置元素, 多了用默认值初始化, 少了去掉末尾的
+    void empty();           // 是否为空
+    size_t size();          // 元素个数
+    size_t max_size();      // 当前设备可以存放最多的元素个数(和设备有关)
+};
+
+class string
+{
+public:
+    /*
+       字符串操作相关
+    */
+
+    // 正向查找字串：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    size_t find(const char &c, size_t pos = 0);
+    size_t find(const char &c, size_t pos, size_t n);
+    size_t find(const string &str, size_t pos = 0);
+    size_t find(const string &str, size_t pos, size_t n);
+    // 反向查找字串：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    // 注意：反向查找中，给的参数也都是正向的参数，如: 0 表示开始位置索引
+    size_t rfind(const char &c, size_t pos = 0);
+    size_t rfind(const char &c, size_t pos, size_t n);
+    size_t rfind(const string &str, size_t pos = 0);
+    size_t rfind(const string &str, size_t pos, size_t n);
+    // 正向查找字符：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    // 只要找到给定字符串中的任何一个字符时，就匹配成功(相当于是模糊匹配了)
+    size_t find_first_of(const char &c, size_t pos = 0);
+    size_t find_first_of(const char &c, size_t pos, size_t n);
+    size_t find_first_of(const string &str, size_t pos = 0);
+    size_t find_first_of(const string &str, size_t pos, size_t n);
+    // 反向查找字符：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    // 只要找到给定字符串中的任何一个字符时，就匹配成功(相当于是模糊匹配了)
+    size_t find_last_of(const char &c, size_t pos = 0);
+    size_t find_last_of(const char &c, size_t pos, size_t n);
+    size_t find_last_of(const string &str, size_t pos = 0);
+    size_t find_last_of(const string &str, size_t pos, size_t n);
+    // 正向查找不存在的字符：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    // 只要找到给定字符串中的任何一个不存在的字符时，就匹配成功(相当于是找不存在的了)
+    size_t find_first_not_of(const char &c, size_t pos = 0);
+    size_t find_first_not_of(const char &c, size_t pos, size_t n);
+    size_t find_first_not_of(const string &str, size_t pos = 0);
+    size_t find_first_not_of(const string &str, size_t pos, size_t n);
+    // 反向查找不存在的字符：<目标字符/目标字符串> <开始查找的位置(索引)> <查找目标字符串中的前几个字符>
+    // 只要找到给定字符串中的任何一个不存在的字符时，就匹配成功(相当于是找不存在的了)
+    size_t find_last_not_of(const char &c, size_t pos = 0);
+    size_t find_last_not_of(const char &c, size_t pos, size_t n);
+    size_t find_last_not_of(const string &str, size_t pos = 0);
+    size_t find_last_not_of(const string &str, size_t pos, size_t n);
+};
